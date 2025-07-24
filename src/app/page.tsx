a698a7168e422b888a9d40e11838f4c5871b4a80
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-07-24 12:12:27 -0700

    Update home page content and styling
    - Change text content to be more descriptive
    - Add minimalist styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
-    <section>
-      <h1 className="text-3xl">Anrey Wang's Personal Website</h1>
-    </section>
+    <main className="flex justify-center items-center h-screen w-screen px-12">
+      <h1 className="text-xl lg:text-3xl">Anrey Wang's personal website is under construction.</h1>
+    </main>
   );
 }

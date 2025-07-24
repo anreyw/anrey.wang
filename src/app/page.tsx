98aa881af2afbfb1be56f5d58c35c6418b61b6c3
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-07-24 12:21:07 -0700

    Hotfix: escape apostrophe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <main className="flex justify-center items-center h-screen w-screen px-12">
-      <h1 className="text-xl lg:text-3xl">Anrey Wang's personal website is under construction.</h1>
+      <h1 className="text-xl lg:text-3xl">Anrey Wang&apos;s personal website is under construction.</h1>
     </main>
   );
 }

5d2bc0323865abbd5b2ffb0fc4306c987216739f
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-07-26 12:38:43 -0700

    Apply prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import { JSX } from 'react'
+import { JSX } from "react";
 
 export default function Page(): JSX.Element {
   return (
     <main className="flex flex-col justify-center items-center h-screen w-screen px-12 gap-2">
-      <h1 className="text-xl lg:text-3xl">
-        Anrey Wang
-      </h1>
+      <h1 className="text-xl lg:text-3xl">Anrey Wang</h1>
       <p className="font-mono text-2xl">This is mono text.</p>
     </main>
   );

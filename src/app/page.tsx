06f4e5ad4930a68e820067ef9ba5e5183a51322a
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-07-25 18:12:25 -0700

    Add text Tailwind class for Geist font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,11 @@ import { JSX } from 'react'
 
 export default function Page(): JSX.Element {
   return (
-    <main className="flex justify-center items-center h-screen w-screen px-12">
+    <main className="flex flex-col justify-center items-center h-screen w-screen px-12 gap-2">
       <h1 className="text-xl lg:text-3xl">
-        Anrey Wang&apos;s personal website is under construction.
+        Anrey Wang
       </h1>
+      <p className="font-mono text-2xl">This is mono text.</p>
     </main>
   );
 }

62bdb2596ddfc0d179b1f42e445177b0d5ba46b1
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-07-26 14:27:18 -0700

    Change home page text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,7 @@ import type { JSX } from "react";
 export default function Page(): JSX.Element {
   return (
     <main className="flex flex-col justify-center items-center h-screen w-screen px-12 gap-2">
-      <h1 className="text-xl lg:text-3xl">Anrey Wang</h1>
-      <p className="font-mono text-2xl">This is mono text.</p>
+      <p className="text-xl lg:text-3xl">Anrey Wang, a spirit ditty of no tone.</p>
     </main>
   );
 }

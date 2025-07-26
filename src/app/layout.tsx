74dec39696e26575f075c386fb4ee5da2e396de2
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-07-25 18:10:32 -0700

    Import Geist font to root layout
    - import from geist packages
    - expose CSS variables within html tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,17 @@
 import "./globals.css";
 
+// == FONTS ==
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
+      <body className="font-sans">{children}</body>
     </html>
   );
 }

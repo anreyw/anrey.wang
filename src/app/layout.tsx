211559d64a9b3c18b3268a20d40fb69307f1780d
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-07-26 13:49:38 -0700

    Remove unnecessary font Tailwind class

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
-      <body className="font-sans">{children}</body>
+      <body>{children}</body>
     </html>
   );
 }

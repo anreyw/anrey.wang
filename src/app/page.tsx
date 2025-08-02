183db51c601a90ee1c0f18a4755be9e6fb74b24f
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-08-02 16:27:56 -0700

    Change text content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ export default function Page(): JSX.Element {
   return (
     <main className="flex h-screen w-screen flex-col items-center justify-center gap-2 px-12">
       <p className="text-xl lg:text-3xl">
-        Anrey Wang enjoys deep conversations on long walks.
+        Anrey Wang enjoys deep conversations and long walks (often at the same
+        time).
       </p>
     </main>
   );

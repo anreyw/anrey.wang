d505121143f70682c05aac2465c7e778e8c1e36b
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-08-09 21:29:20 -0700

    Add avatar image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+import Image from "next/image";
 import type { JSX } from "react";
 
 export default function Page(): JSX.Element {
   return (
-    <main className="flex h-screen w-screen flex-col items-center justify-center gap-2 px-12">
+    <main className="flex h-screen w-screen flex-col items-center justify-center gap-12 px-12">
+      <Image
+        src="/anrey-avatar.png"
+        alt="Anrey Wang"
+        width={1024}
+        height={1024}
+        className="max-w-md rounded-full"
+      />
       <p className="text-xl lg:text-3xl">
         Anrey Wang enjoys deep conversations and long walks (often at the same
         time).

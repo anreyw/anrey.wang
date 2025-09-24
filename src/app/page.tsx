1e6dfff535c46c294969256e6395ac4afa0734ee
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-09-24 12:06:01 -0700

    Fix unescaped apostrophe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import type { JSX } from "react";
 export default function Page(): JSX.Element {
   return (
     <div className="mt-48 flex justify-center px-12">
-      <p>My name is Anrey Wang, and I'm unsure what to put here.</p>
+      <p>My name is Anrey Wang, and I&apos;m unsure what to put here.</p>
     </div>
   );
 }

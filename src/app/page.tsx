89931f12f4b7115f98d5da94c1d191f2dd135364
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-09-26 09:38:09 -0700

    Update homepage content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,15 @@ import type { JSX } from "react";
 export default function Page(): JSX.Element {
   return (
     <div className="mt-48 flex justify-center px-12">
-      <p>My name is Anrey Wang, and I&apos;m unsure what to put here.</p>
+      <p className="text-4xl" lang="zh">
+        不立文字
+        <br />
+        教外別傳
+        <br />
+        直指人心
+        <br />
+        見性成佛
+      </p>
     </div>
   );
 }

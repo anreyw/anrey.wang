b7db868fa28082117fa12cd0c458324fbbc20e48
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-10-27 11:33:53 -0700

    Add ruby text to one character
    To test usage of ruby text on Chinese characters.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Page(): JSX.Element {
   return (
     <div className="mt-48 flex justify-center px-12">
       <p className="text-4xl" lang="zh">
-        不立文字
+        <ruby>不<rt>bú</rt></ruby>立文字
         <br />
         教外別傳
         <br />

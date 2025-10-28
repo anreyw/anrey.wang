e5dffdf0365a05eb17d227a741e229c4f08b928f
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-10-28 14:54:59 -0700

    Switch order of lines
    To better correspond with English and Japanese conventions.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ export default function Page(): JSX.Element {
   return (
     <div className="mt-48 flex justify-center px-12">
       <p className="text-4xl" lang="zh">
-        <ruby>不<rt>bú</rt></ruby>立文字
+        教外別傳        
         <br />
-        教外別傳
+	<ruby>不<rt>bú</rt></ruby>立文字
         <br />
         直指人心
         <br />

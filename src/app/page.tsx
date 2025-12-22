ef0285f6d1b598d180461bb7fdfe23ac54508875
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-12-22 09:10:23 -0800

    Change main content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,75 +2,9 @@ import type { JSX } from "react";
 
 export default function Page(): JSX.Element {
   return (
-    <main className="mt-48 flex justify-center px-12">
-      <p className="text-4xl" lang="zh">
-        <ruby>
-          <span title="teaching">教</span>
-          <rt>jiào</rt>
-        </ruby>
-        <ruby>
-          <span title="outside">外</span>
-          <rt>wài</rt>
-        </ruby>
-        <ruby>
-          <span title="separate">別</span>
-          <rt>bié</rt>
-        </ruby>
-        <ruby>
-          <span title="transmission">傳</span>
-          <rt>chuán</rt>
-        </ruby>
-        <br />
-        <ruby>
-          <span title="not">不</span>
-          <rt>bú</rt>
-        </ruby>
-        <ruby>
-          <span title="establish">立</span>
-          <rt>lì</rt>
-        </ruby>
-        <ruby>
-          <span title="language">文</span>
-          <rt>wén</rt>
-        </ruby>
-        <ruby>
-          <span title="text">字</span>
-          <rt>zì</rt>
-        </ruby>
-        <br />
-        <ruby>
-          <span title="directly">直</span>
-          <rt>zhí</rt>
-        </ruby>
-        <ruby>
-          <span title="point at">指</span>
-          <rt>zhǐ</rt>
-        </ruby>
-        <ruby>
-          <span title="human">人</span>
-          <rt>rén</rt>
-        </ruby>
-        <ruby>
-          <span title="mind">心</span>
-          <rt>xīn</rt>
-        </ruby>
-        <br />
-        <ruby>
-          <span title="see / realize">見</span>
-          <rt>jiàn</rt>
-        </ruby>
-        <ruby>
-          <span title="(true) nature">性</span>
-          <rt>xìng</rt>
-        </ruby>
-        <ruby>
-          <span title="become / attain">成</span>
-          <rt>chéng</rt>
-        </ruby>
-        <ruby>
-          <span title="Buddha">佛</span>
-          <rt>fó</rt>
-        </ruby>
+    <main className="mt-36">
+      <p className="text-center">
+        I would like to eventually put something interesting here.
       </p>
     </main>
   );

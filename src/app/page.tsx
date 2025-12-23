3b8698a59843fa5846e4a558a7711e9792a3d014
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-12-23 09:40:03 -0800

    Update content in <main>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Page(): JSX.Element {
   return (
     <main className="mt-36">
       <p className="text-center">
-        I would like to eventually put something interesting here.
+        I would like to eventually put something interesting here. Maybe some essays?
       </p>
     </main>
   );
